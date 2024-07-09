1912dd2c7ba22f85d4d357679a8a3d85fe328e95
Vishal Rathod <vishalrathod191918@gmail.com>
2024-07-09 12:35:05 +0530

    Palindromic Substring

---
+++ C++/Leetcode/palindromicSubstring.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int expandAroundIndex(string s, int i, int j ){
+    int count=0;
+    while(i>=0 && j<s.length() && s[i] == s[j]){
+        count++;
+        i--;
+        j++;
+    }
+    return count;
+}
+
+
+int countSunstrstrings(string s){
+    int count=0;
+    int n= s.length();
+    for(int i=0; i<n ; i++){
+        // odd
+        int oddKaAns = expandAroundIndex(s, i , i );
+        count = count + oddKaAns;
+        // even
+        int evenKaAns = expandAroundIndex(s, i,i+1);
+        count = count +evenKaAns;
+    }
+    return count;
+}
+
+int main(){
+    string s = "abc";
+   int find = countSunstrstrings(s);
+   cout<<find;
+    
+}
