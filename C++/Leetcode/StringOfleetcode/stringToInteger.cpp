2250d5f05a1a3cb4dd1bc69555a15b20e86c4098
Vishal Rathod <vishalrathod191918@gmail.com>
2024-07-14 23:59:09 +0530

    Leetcode

---
+++ C++/Leetcode/StringOfleetcode/stringToInteger.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int myAtoi(string s){
+    int data = atoi();
+
+}
+
+int main(){
+    string s = "42";
+
+    int ans = myAtoi(s);
+    cout<<ans;
+}
