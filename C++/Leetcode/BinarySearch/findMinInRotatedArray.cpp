69781834c756ef378ce44f078cd83e46a06a0fab
Vishal Rathod <vishalrathod191918@gmail.com>
2024-07-17 17:32:44 +0530

    Leetcode Q no:153

--- C++/Leetcode/BinarySearch/findMinInRotatedArray.cpp
+++ C++/Leetcode/BinarySearch/findMinInRotatedArray.cpp
@@ -10,21 +10,32 @@ int findMin(vector<int>nums){
     int end= nums.size()-1;
     int mid= start + (end - start)/2;
 
+    if(nums[start] <= nums[end]){
+        return nums[start];
+    }
+
     while(start<=end){
-        if(nums[start] < nums[mid]){
-            if(nums[start] < mid){
-                min = nums[start];
-            }
+        if(nums[mid] > nums[mid+1]){
+           return nums[mid+1];
+        }
+        if(nums[mid-1] > nums[mid]){
+            return nums[mid];
+        }
+           
+            
+        if(nums[mid] > nums[start]){
+            start = mid+1;
         }
         else{
-            
+            end = mid -1;
         }
+        mid= start + (end - start)/2;
     }
     return min;
 }
 
 int main(){
-    vector<int>nums{3,4,5,1,2};
+    vector<int>nums{4,5,6,7,8,0,1,2};
 
     int find = findMin(nums);
     cout<<find;
